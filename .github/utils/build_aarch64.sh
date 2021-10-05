45d4938d1719a74f27b1448eb824c15608048dcc
Aaron Smith <aaronreidsmith@gmail.com>
2021-10-05 13:24:40 -0500

    Learn to type, you absolute buffoon

--- .github/utils/build_aarch64.sh
+++ .github/utils/build_aarch64.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 # Rename/clean input variables
-echo "$@"
 PYTHON_VERSION=$(echo $1 | grep -o -E '[0-9]+' | head -1)
-echo "PYTHON_VERSION=${PYTHON_VERSION}"
 PYTHON=$2
-echo "PYTHON=${PYTHON}"
 
 # Install miniconda for our platform
-curl -s https://repo.anaconda.com/miniconda/Miniconda3-py${PYTHON_VER}_4.10.3-Linux-aarch64.sh | /bin/bash
+curl -s https://repo.anaconda.com/miniconda/Miniconda3-py${PYTHON_VERSION}_4.10.3-Linux-aarch64.sh | /bin/bash
 conda --version
