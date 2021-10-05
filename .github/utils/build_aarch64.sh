e65ed943bc2f1c371e9df009223ffdc4be867428
Aaron Smith <aaronreidsmith@gmail.com>
2021-10-05 13:03:20 -0500

    fml

--- .github/utils/build_aarch64.sh
+++ .github/utils/build_aarch64.sh
@@ -5,5 +5,5 @@ PYTHON_VERSION=$(echo $1 | awk -F'[^0-9]+' '{ print $2 }')
 PYTHON=$2
 
 # Install miniconda for our platform
-curl -s https://repo.anaconda.com/miniconda/Miniconda3-py${PY_VER}_4.10.3-Linux-aarch64.sh | /bin/bash
+curl -s https://repo.anaconda.com/miniconda/Miniconda3-py${PYTHON_VER}_4.10.3-Linux-aarch64.sh | /bin/bash
 conda --version
