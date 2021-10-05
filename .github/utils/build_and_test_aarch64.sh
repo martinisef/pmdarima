d7bb2703d90a435ebff8d7a84853fc22c6af658e
Aaron Smith <aaronreidsmith@gmail.com>
2021-10-05 14:43:08 -0500

    See if we can get the right numpy...

--- .github/utils/build_and_test_aarch64.sh
+++ .github/utils/build_and_test_aarch64.sh
@@ -6,13 +6,16 @@ PYTHON_VERSION=$1
 apt-get update
 apt-get install -y build-essential
 
+# Init our shell
+conda init bash
+
 # Create env
-conda create --name build-env --python=$PYTHON_VERSION -y
+conda create -y --name build-env python=$PYTHON_VERSION
 conda activate build-env
 
 # Install requirements
-conda install --file build_tools/build_requirements.txt
-conda install --freeze-installed --file requirements.txt
+conda install -y --file build_tools/build_requirements.txt
+conda install -y --freeze-installed --file requirements.txt
 
 # Create wheel
 python setup.py bdist_wheel
