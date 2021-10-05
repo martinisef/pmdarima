e1f118c99e1be4dfd544dc80283157d0d1692560
Aaron Smith <aaronreidsmith@gmail.com>
2021-10-05 13:50:50 -0500

    Try continuum image?

--- .github/utils/build_aarch64.sh
+++ .github/utils/build_aarch64.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Rename/clean input variables
-PYTHON_VERSION=$(echo $1 | grep -o -E '[0-9]+' | head -1)
-PYTHON=$2
-
-# Install miniconda for our platform
-curl https://repo.anaconda.com/miniconda/Miniconda3-py${PYTHON_VERSION}_4.10.3-Linux-aarch64.sh -o ~/miniconda.sh
-/bin/bash ~/miniconda.sh -b -p $HOME/miniconda
 conda --version
+which python
