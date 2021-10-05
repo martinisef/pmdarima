4561b37bc28ada517c487cbd5c5797c95a6b3716
Aaron Smith <aaronreidsmith@gmail.com>
2021-10-05 13:22:55 -0500

    Debug

--- .github/utils/build_aarch64.sh
+++ .github/utils/build_aarch64.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Rename/clean input variables
-PYTHON_VERSION=$(echo $1 | awk -F'[^0-9]+' '{ print $2 }')
+echo "$@"
+PYTHON_VERSION=$(echo $1 | grep -o -E '[0-9]+' | head -1)
+echo "PYTHON_VERSION=${PYTHON_VERSION}"
 PYTHON=$2
+echo "PYTHON=${PYTHON}"
 
 # Install miniconda for our platform
 curl -s https://repo.anaconda.com/miniconda/Miniconda3-py${PYTHON_VER}_4.10.3-Linux-aarch64.sh | /bin/bash
