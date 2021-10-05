d66643d1410db7e488db4761af709d3b3f827083
Aaron Smith <aaronreidsmith@gmail.com>
2021-10-05 15:13:20 -0500

    Fix numpy pin

--- .github/utils/build_and_test_aarch64.sh
+++ .github/utils/build_and_test_aarch64.sh
@@ -4,7 +4,7 @@ PYTHON_VERSION=$1
 
 # Make sure we have gcc installed
 apt-get update
-apt-get install -y build-essential
+apt-get install -y build-essential python-dev
 
 # Init our shell
 conda init bash
@@ -16,7 +16,7 @@ conda activate build-env
 
 # Install requirements
 conda config --add channels conda-forge
-conda install -c conda-forge "numpy==1.19" # Preinstall numpy to get right version
+conda install -c conda-forge "numpy>=1.19.3,<1.20" # Preinstall numpy to get right version
 conda install -y --freeze-installed --file build_tools/build_requirements.txt
 conda install -y --freeze-installed --file requirements.txt
 
